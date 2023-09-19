155b19e41a51fa7f49965977d202caf52bb87087
verbess <shiminw@outlook.com>
2023-09-20 06:57:33 +0800

    feat: add interface IUpdatable

---
+++ src/IUpdatable.cs
@@ -0,0 +1,14 @@
+namespace Snake;
+
+/// <summary>
+/// Exposes a method that updates a updatable <see cref="Module" />.
+/// </summary>
+internal interface IUpdatable
+{
+    /// <summary>
+    /// Updates the module with the specified elapsed time.
+    /// </summary>
+    /// <param name="logicalElapse">The logical elapsed time in seconds.</param>
+    /// <param name="realElapse">The real elapsed time in seconds.</param>
+    internal void Update(double logicalElapse, double realElapse);
+}
