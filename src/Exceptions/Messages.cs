ec4bdf0d1ee69be5fb2dea44a71c4226df0e1900
verbess <shiminw@outlook.com>
2023-09-20 07:04:33 +0800

    feat: add class Messages

---
+++ src/Exceptions/Messages.cs
@@ -0,0 +1,14 @@
+namespace Snake;
+
+/// <summary>
+/// Represents a resource class for exception error message constants.
+/// </summary>
+internal static class Messages
+{
+    #region Snake exceptions
+    /// <summary>
+    /// Snake framework error.
+    /// </summary>
+    internal const string Snake_FrameworkException = @"Snake framework error.";
+    #endregion
+}
