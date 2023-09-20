6bbe0823218863363a7951771638144e0b710245
verbess <shiminw@outlook.com>
2023-09-20 11:24:48 +0800

    feat: add message constant

--- src/Core/Exceptions/Messages.cs
+++ src/Core/Exceptions/Messages.cs
@@ -11,4 +11,11 @@ internal static class Messages
     /// </summary>
     internal const string Snake_Exception = @"Snake framework error.";
     #endregion
+
+    #region Argument exceptions
+    /// <summary>
+    /// Type passed must be an interface.
+    /// </summary>
+    internal const string Argument_MustBeInterface = @"Type passed must be an interface.";
+    #endregion
 }
