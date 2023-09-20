e44c7322cefd620cbafa2cba5f0288b82169f9bc
王世民 <shiminw@outlook.com>
2023-09-20 10:20:44 +0800

    feat: add Core module (#2)
    * feat: add class Module
    
    * feat: add interface IUpdatable
    
    * feat: add class Messages
    
    * refactor: short const name
    
    * feat: add class SnakeException

---
+++ src/Exceptions/Messages.cs
@@ -0,0 +1,14 @@
+namespace Snake;
+
+/// <summary>
+/// Represents a resource class for exception error message constants.
+/// </summary>
+internal static class Messages
+{
+    #region Snake exceptions
+    /// <summary>
+    /// Snake framework error.
+    /// </summary>
+    internal const string Snake_Exception = @"Snake framework error.";
+    #endregion
+}
---
+++ src/Exceptions/SnakeException.cs
@@ -0,0 +1,43 @@
+using System.Runtime.Serialization;
+
+namespace Snake;
+
+/// <summary>
+/// Serves as the base class for snake exceptions namespace.
+/// </summary>
+[Serializable]
+public class SnakeException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SnakeException" /> class.
+    /// </summary>
+    public SnakeException()
+        : base(Messages.Snake_Exception) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SnakeException" /> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    public SnakeException(string? message)
+        : base(message) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SnakeException" /> class with a specified error message and a
+    /// reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="innerException">
+    /// The exception that is the cause of the current exception. If the <paramref name="innerException" /> is not a
+    /// null reference, the current exception is raised in a catch block that handles the inner exception.
+    /// </param>
+    public SnakeException(string? message, Exception? innerException)
+        : base(message, innerException) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SnakeException" /> class with serialized data.
+    /// </summary>
+    /// <param name="info">The object that holds the serialized object data.</param>
+    /// <param name="context">The contextual information about the source or destination.</param>
+    protected SnakeException(SerializationInfo info, StreamingContext context)
+        : base(info, context) { }
+}
