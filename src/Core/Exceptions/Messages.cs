3ba528118c6e9a0d0af75df6c18de4f7d9bc55ff
王世民 <shiminw@outlook.com>
2023-09-22 02:32:42 +0800

    feat: add framework entry point (#4)
    * feat: add message constant
    
    * feat: add message constants
    
    * feat: add class SnakeBite
    
    * refactor: modify comments

--- src/Core/Exceptions/Messages.cs
+++ src/Core/Exceptions/Messages.cs
@@ -10,5 +10,24 @@ internal static class Messages
     /// Snake framework error.
     /// </summary>
     internal const string Snake_Exception = @"Snake framework error.";
+    /// <summary>
+    /// Module '{0}' was not found.
+    /// </summary>
+    internal const string Snake_ModuleNotFound = @"Module '{0}' was not found.";
+    /// <summary>
+    /// Type '{0}' is not a standard Snake module.
+    /// </summary>
+    internal const string Snake_NotStandardModule = @"Type '{0}' is not a standard Snake module.";
+    /// <summary>
+    /// Module '{0}' creation failed.
+    /// </summary>
+    internal const string Snake_ModuleCreationFailed = @"Module '{0}' creation failed.";
+    #endregion
+
+    #region Argument exceptions
+    /// <summary>
+    /// Type passed must be an interface.
+    /// </summary>
+    internal const string Argument_MustBeInterface = @"Type passed must be an interface.";
     #endregion
 }
