ad2a6bd13702947eae41753986e731fd8f0a9ade
verbess <shiminw@outlook.com>
2025-03-28 03:27:58 +0800

    feat: add comments

--- src/core/data_pool/DataPool.cs
+++ src/core/data_pool/DataPool.cs
@@ -10,6 +10,16 @@ internal sealed class DataPool
 {
     private readonly ConcurrentDictionary<Type, DataContainer> _containers = [];
 
+    /// <summary>
+    /// Attempts to get the data associated with the specified key from the <see cref="DataPool" />.
+    /// </summary>
+    /// <param name="type">The type of the data to get.</param>
+    /// <param name="key">The key of the data to get.</param>
+    /// <param name="data">
+    /// When this method returns, contains the data obtained from the <see cref="DataPool" /> that has the specified
+    /// key, or the default value of the <see cref="IKeyed" /> if the operation failed.
+    /// </param>
+    /// <returns><c>true</c> if the key was found in the <see cref="DataPool" />; otherwise, <c>false</c>.</returns>
     internal bool TryGetData(Type type, string key, [MaybeNullWhen(false)] out IKeyed data)
     {
         ArgumentNullException.ThrowIfNull(type);
@@ -21,6 +31,14 @@ internal sealed class DataPool
         return container?.TryGetData(key, out data) ?? false;
     }
 
+    /// <summary>
+    /// Attempts to add a data to the <see cref="DataPool" />.
+    /// </summary>
+    /// <param name="data">The data to add.</param>
+    /// <returns>
+    /// <c>true</c> if the data was added to the <see cref="DataPool" /> successfully; <c>false</c> if the data already
+    /// exists.
+    /// </returns>
     internal bool TryAdd(IKeyed data)
     {
         ArgumentNullException.ThrowIfNull(data);
@@ -28,6 +46,13 @@ internal sealed class DataPool
         return _containers.GetOrAdd(data.GetType(), _ => new DataContainer()).TryAdd(data);
     }
 
+    /// <summary>
+    /// Attempts to remove a series of data that has the specified type from the <see cref="DataPool" />.
+    /// </summary>
+    /// <param name="type">The type of the data to remove.</param>
+    /// <returns>
+    /// <c>true</c> if the data were removed from the <see cref="DataPool" /> successfully; otherwise, <c>false</c>.
+    /// </returns>
     internal bool TryRemove(Type type)
     {
         ArgumentNullException.ThrowIfNull(type);
@@ -35,6 +60,18 @@ internal sealed class DataPool
         return _containers.TryRemove(type, out _);
     }
 
+    /// <summary>
+    /// Attempts to remove and return the data that has the specified key from the <see cref="DataPool" />.
+    /// </summary>
+    /// <param name="type">The type of the data to remove and return.</param>
+    /// <param name="key">The key of the data to remove and return.</param>
+    /// <param name="data">
+    /// When this method returns, contains the data removed from the <see cref="DataPool" /> that has the specified
+    /// key, or the default value of the <see cref="IKeyed" /> if the operation failed.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if the data was removed from the <see cref="DataPool" /> successfully; otherwise, <c>false</c>.
+    /// </returns>
     internal bool TryRemove(Type type, string key, [MaybeNullWhen(false)] out IKeyed data)
     {
         ArgumentNullException.ThrowIfNull(type);
@@ -46,6 +83,9 @@ internal sealed class DataPool
         return container?.TryRemove(key, out data) ?? false;
     }
 
+    /// <summary>
+    /// Clears all keys and data from the <see cref="DataPool" />.
+    /// </summary>
     internal void Clear() => _containers.Clear();
 
     /// <summary>
